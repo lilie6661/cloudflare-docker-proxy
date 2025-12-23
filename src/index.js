e465d27a6f8cfc9d1cf75eadc4fc36be50c89c74
lilie6661 <3077723217@qq.com>
2025-12-24 01:13:21 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker.lijie.link]: dockerHub,
+  ["quay.lijie.link]: "https://quay.io",
+  ["gcr.lijie.link]: "https://gcr.io",
+  ["k8s-gcr.lijie.link]: "https://k8s.gcr.io",
+  ["k8s.lijie.link]: "https://registry.k8s.io",
+  ["ghcr.lijie.link]: "https://ghcr.io",
+  ["cloudsmith.lijie.link]: "https://docker.cloudsmith.io",
+  ["ecr.lijie.link]: "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  ["docker-staging.lijie.link]: dockerHub,
 };
 
 function routeByHosts(host) {
